1431ee876790a7a3d679e19e81f768c477f07c4a
eunseo-ko <98506097+eunseo-ko@users.noreply.github.com>
2022-02-09 17:19:04 +0900

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,7 @@
 /*!
-* Start Bootstrap - Landing Page v6.0.4 (https://startbootstrap.com/theme/landing-page)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
-/*!
  * Start Bootstrap - Landing Page v6.0.4 (https://startbootstrap.com/theme/landing-page)
  * Copyright 2013-2021 Start Bootstrap
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
  */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-
-//Get the button:
-mybutton = document.getElementById('myBtn');
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = 'block';
-  } else {
-    mybutton.style.display = 'none';
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
